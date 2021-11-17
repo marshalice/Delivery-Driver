e025f7c9b3f2a80aea1ae729bb209394b5458628
marshalice <alicekramers@outlook.com>
2021-11-17 17:20:34 +0100

    Added trigger

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I've McFallen!");
     }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Step on me uwu");
+    }
 }
