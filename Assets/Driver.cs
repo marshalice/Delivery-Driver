cfddf24225a0d5164488eee93373b62849e5a959
marshalice <alicekramers@outlook.com>
2021-11-17 17:04:00 +0100

    Changed speed to be framerate-independent

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 150f;
+    [SerializeField] float moveSpeed = 10f;
 
     void Start()
     {
@@ -14,8 +14,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-       float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-       float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+       float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+       float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
        transform.Rotate(0, 0, -steerAmount);
        transform.Translate(0, moveAmount, 0);
     }
