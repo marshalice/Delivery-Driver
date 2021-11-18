4eee5479faede234253a1a21d67b611c149aa336
marshalice <alicekramers@outlook.com>
2021-11-18 17:26:10 +0100

    Added check to see if package was picked up

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I've McFallen!");
     }
@@ -11,10 +13,12 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Package"){
             Debug.Log("Package picked up");
+            hasPackage = true;
         }
 
-        if(other.tag == "Rebel"){
+        if(other.tag == "Rebel" && hasPackage){
             Debug.Log("Delivered package.");
+            hasPackage = false;
         }
     }
 }
