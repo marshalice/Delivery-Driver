0cdebbb5c92daa463d9f67cc9f97b103dc505cc3
marshalice <alicekramers@outlook.com>
2021-11-18 17:22:17 +0100

    Added package and rebel objects and detection of tags

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("I've McFallen!");
     }
     
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Step on me uwu");
+        if(other.tag == "Package"){
+            Debug.Log("Package picked up");
+        }
+
+        if(other.tag == "Rebel"){
+            Debug.Log("Delivered package.");
+        }
     }
 }
