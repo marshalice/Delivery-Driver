8766d7025ccd57c8684de78293a5c518ba4aba18
marshalice <alicekramers@outlook.com>
2021-11-18 17:46:35 +0100

    Made car change colour when picking up packages

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     bool hasPackage;
     [SerializeField] float delay = 1f;
 
@@ -15,12 +24,14 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !hasPackage){
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, delay);
         }
 
         if(other.tag == "Rebel" && hasPackage){
             Debug.Log("Delivered package.");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
